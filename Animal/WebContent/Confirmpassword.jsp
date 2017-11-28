85c314531455a88c14fb4f1dec30a837a8502485
jaeyo <jaeyo@192.168.0.12>
2017-11-29 02:39:26 +0900

    크크크크

--- Animal/WebContent/Confirmpassword.jsp
+++ Animal/WebContent/Confirmpassword.jsp
@@ -7,14 +7,14 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  
   <title>login</title>
   <!-- Bootstrap core CSS-->
  <link href="./Resources/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="./Resources/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <link href="./Resources/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
   <link href="./Resources/css/sb-admin.css" rel="stylesheet">
+  <link href="css/bootstrap.css" rel="stylesheet">
 </head>
 <body class="bg-dark">
   <div class="container">
--- Animal/src/animal/control/Controller.java
+++ Animal/src/animal/control/Controller.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpSession;
 
 import animal.bean.BoardDBBean;
 import animal.bean.BoardDataBean;
+import animal.bean.UserDBBean;
 
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
@@ -22,52 +23,88 @@ public class Controller extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String src = "";
 		String address = null;
 		request.setCharacterEncoding("utf-8");
 		HttpSession session = request.getSession();
 		String action = request.getParameter("action");
 		BoardDBBean board = BoardDBBean.getinstance();
+		UserDBBean user = UserDBBean.getinstance();
 		
+		//action이 null이 아닐 경우에만 수행
 		if(action != null) {
-			
+		
+		//로그인폼을 매칭시켜주는 부분
 		if(action.equals("login")) {
 			address = "login.jsp";
 		}
 		
+		//회원가입폼을 매칭시켜주는 부분
 		else if(action.equals("register")) {
 			address = "register.jsp";
 		}
 		
+		//로그아웃 시켜주는 부분
 		else if(action.equals("logout")) {
 			session.invalidate();
 			address = "index.jsp";
 		}
 		
+		//로그인을 처리해주는 부분
 		else if(action.equals("login_comp")) {
-			//request.setAttribute("user_id", request.getParameter("user_id"));
-			//request.setAttribute("user_pw", request.getParameter("user_pw"));
 			String user_id = request.getParameter("user_id");
 			String user_pw = request.getParameter("user_pw");
+			
+			if(user_id == null || user_id.equals("") || user_pw == null || user_pw.equals("")) {
+				request.getSession().setAttribute("messageType", "오류 메시지");
+				request.getSession().setAttribute("messageContent", "모든 내용을 입력하세요.");
+				address = "login.jsp";
+			}
+			
+			else {
+				int result = user.login(user_id, user_pw);
+				
+				if(result == 1) {
+					request.getSession().setAttribute("user_id", user_id);
+					address = "index.jsp";
+				}
+				
+				else if(result == 0) {
+					request.getSession().setAttribute("messageType", "오류 메세지");
+					request.getSession().setAttribute("messageContent", "아이디 혹은 비밀번호가 맞지 않습니다.");
+					address = "login.jsp";
+				}
+				
+				else {
+					request.getSession().setAttribute("messageType", "오류 메시지");
+					request.getSession().setAttribute("messageContent", "내부적인 오류입니다. 다시 시도해 주세요.");
+					address = "login.jsp";
+				}
+			}
+			
 			address = "LoginAction?user_id="+user_id+"&user_pw="+user_pw;
 		}
 		
+		//글쓰기폼을 매칭시켜주는 부분
 		else if (action.equals("write")) {
 			address = "write.jsp";
 		}
 		
+		//비밀번호 확인을 매칭시켜주는 부분
 		else if (action.equals("confirm")) {
 			address = "Confirmpassword.jsp";
 		}
 		
+		//아이디찾기폼을 매칭시켜주는 부분
 		else if (action.equals("forget_id")) {
 			address = "forget_id.jsp";
 		}
 		
+		//비밀번호찾기 폼을 매칭시켜주는부분
 		else if (action.equals("forget_pw")) {
 			address = "forget_pw.jsp";
 		}
 		
+		//뉴스피드 글쓰기폼을 매칭시켜주는 부분
 		else if(action.equals("news_write")) {
 			address = "news_write.jsp";
 		}
